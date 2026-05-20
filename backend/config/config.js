5101bb3e049d7e78dcb9b428317c3cd795fb273b
rahul karna <rahulkarna889@gmail.com>
2026-05-20 17:18:36 +0545

    feat: implement JWT authentication with access and refresh tokens.

--- backend/config/config.js
+++ backend/config/config.js
@@ -12,6 +12,10 @@ const validateEnvVariables = (varName) => {
 const config = {
     PORT: validateEnvVariables("PORT"),
     MONGODB_URI: validateEnvVariables("MONGODB_URI"),
+    ACCESS_TOKEN_SECRET: validateEnvVariables("ACCESS_TOKEN_SECRET"),
+    ACCESS_TOKEN_EXPIRY: validateEnvVariables("ACCESS_TOKEN_EXPIRY"),
+    REFRESH_TOKEN_SECRET: validateEnvVariables("REFRESH_TOKEN_SECRET"),
+    REFRESH_TOKEN_EXPIRY: validateEnvVariables("REFRESH_TOKEN_EXPIRY"),
 }
 
 export default config
--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,152 @@
+import User from "../models/User.model.js";
+import RefreshToken from "../models/RefreshToken.models.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import config from "../config/config.js";
+
+const generateAccessToken = (payload) => {
+  return jwt.sign(payload, config.ACCESS_TOKEN_SECRET, {
+    expiresIn: config.ACCESS_TOKEN_EXPIRY,
+  });
+};
+
+const generateRefreshToken = (payload) => {
+  return jwt.sign(payload, config.REFRESH_TOKEN_SECRET, {
+    expiresIn: config.REFRESH_TOKEN_EXPIRY,
+  });
+};
+
 export const registerUser = async (req, res) => {
-  res.status(200).json({ message: "Register user endpoint placeholder" });
+  try {
+    const { name, email, password } = req.body;
+
+    //validation
+    if (!name || !email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Please enter valid credentials" });
+    }
+
+    //finding existing one
+    let existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    //hashing
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({ name, email, password: hashPassword });
+
+    const payload = { id: newUser.id };
+    const accessToken = generateAccessToken(payload);
+    const refreshToken = generateRefreshToken(payload);
+
+    await RefreshToken.create({
+      token: refreshToken,
+      userId: newUser.id,
+    });
+
+    return res.status(201).json({
+      message: "User created successfully",
+      user: newUser.name,
+      accessToken,
+      refreshToken,
+    });
+  } catch (err) {
+    return res.status(500).json({ message: "internal server error" });
+  }
 };
 
 export const loginUser = async (req, res) => {
-  res.status(200).json({ message: "Login user endpoint placeholder" });
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Please enter valid credentials" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    //to check if user entered password is correct or not
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const payload = { id: user.id };
+    const accessToken = generateAccessToken(payload);
+    const refreshToken = generateRefreshToken(payload);
+
+    await RefreshToken.create({
+      token: refreshToken,
+      userId: user.id,
+    });
+
+    return res.status(200).json({
+      message: "Login successful",
+      user: user.name,
+      accessToken,
+      refreshToken,
+    });
+  } catch (err) {
+    return res.status(500).json({ message: "internal server error" });
+  }
+};
+
+//getting a new Access Token when expired
+export const newToken = async (req, res) => {
+  try {
+    const { refreshToken } = req.body;
+    if (!refreshToken) {
+      return res.status(403).json({ message: "Invalid Token" });
+    }
+    const refTokenInDb = await RefreshToken.findOne({ token: refreshToken });
+    if (!refTokenInDb) {
+      return res.status(403).json({ message: "Refresh token not found" });
+    }
+
+    jwt.verify(refreshToken, config.REFRESH_TOKEN_SECRET, (err, user) => {
+      //checks if the Refresh Token is expired or not
+      if (err) {
+        return res
+          .status(403)
+          .json({ message: "Refresh token is expired or invalid" });
+      }
+    });
+
+    const payload = { id: refTokenInDb.userId };
+    const newAccessToken = generateAccessToken(payload);
+
+    return res.status(200).json({
+      message: "Access token generated successfully",
+      accessToken: newAccessToken,
+    });
+  } catch (err) {
+    return res.status(500).json({ message: "internal server error" });
+  }
+};
+
+export const logoutUser = async (req, res) => {
+  try {
+    const { refreshToken } = req.body;
+    if (!refreshToken) {
+      return res.status(400).json({ message: "Invalid Token" });
+    }
+
+    const refTokenInDb = await RefreshToken.findOneAndDelete({
+      token: refreshToken,
+    });
+    if (!refTokenInDb) {
+      return res.status(400).json({ message: "Token not found" });
+    }
+    return res.status(200).json({ message: "Logout successful" });
+  } catch (err) {
+    return res.status(500).json({ message: "internal server error" });
+  }
 };
